15c2fb50cab3f4abd9f20e2eccc6ce9767b68929
21wh1a1280 <113002363+21wh1a1280@users.noreply.github.com>
2023-02-04 20:29:21 +0530

    Add files via upload

---
+++ selection sort.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main() {
+   int arr[10]={6,12,0,18,11,99,55,45,34,2};
+   int n=10;
+   int i, j, position, swap;
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
+      }
+   }
+   for (i = 0; i < n; i++)
+      printf("%d\t", arr[i]);
+   return 0;
+}
+/*
+0 2 6 11 12 18 34 45 55 99
+*/
